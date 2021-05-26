68ca4095943fcbb6141d528599de10ca62dc7d95
= <tataratone@gmail.com>
2021-05-26 22:22:22 +0900

    fix 202105250912

--- js/202105250912_000.js
+++ js/202105250912_000.js
@@ -3,8 +3,7 @@ class Ball {
   v = [0, 0];
   col = [0, 0, 0];
 
-  constructor() {
-  }
+  constructor() {}
 
   update_loc() {
     this.loc[0] += this.v[0];
@@ -30,16 +29,19 @@ class Ball {
   display() {
     noStroke();
     fill(this.col[0], this.col[1], this.col[2]);
-    ellipse(this.loc[0], this.loc[1], 5, 5)
+    ellipse(this.loc[0], this.loc[1], 5, 5);
   }
 }
 
 function setup() {
-  createCanvas(300, 300);
-  ball = []
+  createCanvas(windowWidth, windowHeight);
+  ball = [];
   for (let i = 0; i < 10; i++) {
     let obj = new Ball();
-    obj.loc = [rand(width / 4, width * 3 / 4), rand(height / 4, height * 3 / 4)];
+    obj.loc = [
+      rand(width / 4, (width * 3) / 4),
+      rand(height / 4, (height * 3) / 4),
+    ];
     obj.v = [rand(-3, 3), rand(-3, 3)];
     obj.col = [rand(0, 255), rand(0, 255), rand(0, 255)];
     ball.push(obj);
@@ -47,8 +49,8 @@ function setup() {
 }
 
 function rand(a, b) {
-  if (typeof b === 'undefined') {
-    if (typeof a === 'undefined') {
+  if (typeof b === "undefined") {
+    if (typeof a === "undefined") {
       mi = 0;
       ma = 1;
     } else {
@@ -63,10 +65,11 @@ function rand(a, b) {
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight);
   background(0);
   fill(255);
   for (let key in ball) {
     ball[key].display();
+    ball[key].update_loc();
   }
-  for (let key in ball) { ball[key].update_loc(); }
 }
